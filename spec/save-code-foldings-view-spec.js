57d67ede4a21b8967b040852645738db2f3b8aac
Christoph Barosch <barosch.christoph@hotmail.com>
2017-04-27 22:39:31 +0200

    Base commit

---
+++ spec/save-code-foldings-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SaveCodeFoldingsView from '../lib/save-code-foldings-view';
+
+describe('SaveCodeFoldingsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
